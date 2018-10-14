ff417263f017ccf3716c994ad1c170e7044fd911
Bharath <bharathanand000@gmail.com>
2018-10-14 20:48:51 +0530

    chota error correction in displaying object for updateDocument in operations.js

--- operations.js
+++ operations.js
@@ -41,9 +41,9 @@ exports.removeDocument = (db,document,collection,callback) =>{
 
 exports.updateDocument = (db,document,update,collection,callback) => {
     const coll=db.collection(collection);
-    coll.updateOne(document, { $set: update},null, (err,result)=>{
+    coll.updateOne(document, { $set: update },null, (err,result)=>{
         assert.equal(err,null);
-        console.log('Updated the document with '+update);
+        console.log('Updated the document with ',update);
         callback(result);
     });
 };
